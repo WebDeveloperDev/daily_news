11a8f2e83ddbae5da196cf6de794399dc0777e7f
Devendra Singh Solanki <93856267+WebDeveloperDev@users.noreply.github.com>
2024-09-09 09:02:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,23 +11,24 @@ async function sleep(ms) {
 
 async function main(start, end) {
     let newsList = await news.getNewsHeadlines();
-    for (let i = start; i < end; i++) {
-        let newsObj = newsList[i];
-        let imgUrl = newsObj.imgUrl;
-        let heading = newsObj.title;
-        let description = newsObj.description;
-        if (imgUrl != null && heading != null) {
-            console.log(imgUrl);
-            console.log(heading);
-            let status = await editImage.editImage(imgUrl, heading);
-            if (status == true) {
-                await postToInstagram.post('./images/output.jpeg', description);
-                await sleep(5*60 * 1000);
-            }
-            console.log(i);
-            console.log("---------------------------------------------");
-        }
-    }
+    console.log(newsList)
+    // for (let i = start; i < end; i++) {
+    //     let newsObj = newsList[i];
+    //     let imgUrl = newsObj.imgUrl;
+    //     let heading = newsObj.title;
+    //     let description = newsObj.description;
+    //     if (imgUrl != null && heading != null) {
+    //         console.log(imgUrl);
+    //         console.log(heading);
+    //         let status = await editImage.editImage(imgUrl, heading);
+    //         if (status == true) {
+    //             await postToInstagram.post('./images/output.jpeg', description);
+    //             await sleep(5*60 * 1000);
+    //         }
+    //         console.log(i);
+    //         console.log("---------------------------------------------");
+    //     }
+    // }
 }
 
 module.exports = {main};
